280618e06283a25643e6059c5be1c43347ca962b
Lacey Murphy <lneagle@gmail.com>
2026-03-26 13:35:02 -0400

    Added debugging for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,12 +12,64 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.error('Proxy error:', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log(
+              'Sending Request:',
+              req.method,
+              req.url,
+              ' => TO TARGET => ',
+              proxyReq.method,
+              proxyReq.protocol,
+              proxyReq.host,
+              proxyReq.path
+            );
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log(
+              'Received Response from Target:',
+              proxyRes.statusCode,
+              req.url
+            );
+            // Optionally log response headers
+            // console.log(JSON.stringify(proxyRes.headers, null, 2)); 
+          });
+        },
       },
       '/auth': {
         target: 'https://capstone-3-library-app.vercel.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/auth/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.error('Proxy error:', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log(
+              'Sending Request:',
+              req.method,
+              req.url,
+              ' => TO TARGET => ',
+              proxyReq.method,
+              proxyReq.protocol,
+              proxyReq.host,
+              proxyReq.path
+            );
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log(
+              'Received Response from Target:',
+              proxyRes.statusCode,
+              req.url
+            );
+            // Optionally log response headers
+            // console.log(JSON.stringify(proxyRes.headers, null, 2)); 
+          });
+        },
       },
     },
   },
